dc2c1dbfb668c62f6461e7a3836d643471de30ad
Dustin Hays <hays.dustin.u@gmail.com>
2023-05-02 22:35:25 -0700

    ejs and get

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// REQUIRE DEPENDENCIES
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 9005
 require('dotenv').config()
 
+// DECLARED DB VARIABLES
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = "star-trek-aliens",
@@ -12,7 +14,7 @@ let db,
 
 
 
-
+// CONNECT TO MONGODB
 MongoClient.connect(dbConnectionStr)
     .then(client => {
         console.log(`Connected to ${dbName} database`)
@@ -25,6 +27,35 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+
+//CRUD METHODS
+app.get('/',(req, res) => {
+     db.collection('alien-info').find().toArray()
+        .then(data => {
+            let nameList = data.map(item => item.speciesName)
+            console.log(nameList)
+            res.render('index.ejs', {info: nameList})
+
+        })
+        .catch(error => console.log(error))
+
+    //console.log(contents)
+})
+
+app.post('/api', (req, res) => {
+
+})
+
+app.put('/updateEntry', (req, res) => {
+
+})
+
+app.delete('/deleteEntry', (req, res) => {
+
+})
+
+
+//SET UP LOCAL HOST ON PORT
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
